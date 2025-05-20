f16aaf293ac86eaa348fab9dfbe8ee847b64896c
julia souto souza freitas <julia.souto@aluno.ifsp.edu.br>
2025-05-20 10:07:44 -0300

    add a simple json backend

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -1,45 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import React, { Component} from "react";
-import {View, Text, Image, ScrollView, StyleSheet } from 'react-native';
+import ScrollViewExample from './components/ScrollViewExample';
 
-class ScrollViewExample extends Component {
-    state = {
-        names: [
-           
-        ]
-    }
-
-    render() {
-        return (
-            <View>
-                <ScrollView>
-                 {
-                    this.state.names.map((item, index) => (
-                        <View 
-                            key={item.id}
-                            style={styles.item}
-                        >
-                        <Image source={require('../assets/favicon.png')} />
-                        <Text>{item.name}</Text>
-                        </View>
-                    ))
-                 }   
-                </ScrollView>
-            </View>
-        );
-    }
+export default function App() {
+ 
+  return (
+    <View style={styles.container}>
+      <ScrollViewExample />
+    </View>
+  );
 }
-export default ScrollViewExample;
 
 const styles = StyleSheet.create({
-    item: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 30,
-        margin: 2,
-        borderColor: '#2a4944',
-        borderWidth: 1,
-        backgroundColor: '#d2f7f1'
-    }
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 600,
+    marginTop: 150,
+    backgroundColor: '#800080',
+  }
 });
