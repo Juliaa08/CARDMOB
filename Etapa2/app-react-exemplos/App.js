997059ca425c4d8470f0e7803c127ad451183775
julia souto souza freitas <julia.souto@aluno.ifsp.edu.br>
2025-05-20 08:40:28 -0300

    ScrollView: Examples

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -5,18 +5,7 @@ import {View, Text, Image, ScrollView, StyleSheet } from 'react-native';
 class ScrollViewExample extends Component {
     state = {
         names: [
-            { 'name': 'Ben', 'id': 1 },
-            { 'name': 'Susan', 'id': 2},
-            { 'name': 'Robert', 'id': 3},
-            { 'name': 'Mary', 'id': 4},
-            { 'name': 'Daniel', 'id': 5},
-            { 'name': 'Laura', 'id': 6},
-            { 'name': 'John', 'id': 7},
-            { 'name': 'Debra', 'id': 8},
-            { 'name': 'Aron', 'id': 9},
-            { 'name': 'Ann', 'id': 10},
-            { 'name': 'Steve', 'id': 11},
-            { 'name': 'Olivia', 'id': 12},
+           
         ]
     }
 
--- Etapa2/app-react-exemplos/components/ScrollViewExample.js
+++ Etapa2/app-react-exemplos/components/ScrollViewExample.js
@@ -5,18 +5,18 @@ import {View, Text, Image, ScrollView, StyleSheet } from 'react-native';
 class ScrollViewExample extends Component {
     state = {
         names: [
-            { 'name': 'Ben', 'id': 1 },
-            { 'name': 'Susan', 'id': 2},
-            { 'name': 'Robert', 'id': 3},
-            { 'name': 'Mary', 'id': 4},
-            { 'name': 'Daniel', 'id': 5},
-            { 'name': 'Laura', 'id': 6},
-            { 'name': 'John', 'id': 7},
-            { 'name': 'Debra', 'id': 8},
-            { 'name': 'Aron', 'id': 9},
-            { 'name': 'Ann', 'id': 10},
-            { 'name': 'Steve', 'id': 11},
-            { 'name': 'Olivia', 'id': 12},
+            { 'name': 'Benson', 'id': 1 },
+            { 'name': 'Susane', 'id': 2},
+                       { 'name': 'Roberto', 'id': 3},
+                       { 'name': 'Mary', 'id': 4},
+                       { 'name': 'Daniel', 'id': 5},
+                       { 'name': 'Laura', 'id': 6},
+                       { 'name': 'John', 'id': 7},
+                       { 'name': 'Debra', 'id': 8},
+                       { 'name': 'Maria', 'id': 9},
+                       { 'name': 'Livia', 'id': 10},
+                       { 'name': 'Gustavo', 'id': 11},
+                       { 'name': 'Julia', 'id': 12},
         ]
     }
 
