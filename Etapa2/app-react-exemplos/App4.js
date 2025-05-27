0f2a41df8d566ceb2bfb3d198bbac3a32a25dfc9
julia souto souza freitas <julia.souto@aluno.ifsp.edu.br>
2025-05-27 10:15:57 -0300

    Add CRUD with Backend

---
+++ Etapa2/app-react-exemplos/App4.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import ScrollViewExample from './components/ScrollViewExample';
+
+export default function App() {
+ 
+  return (
+    <View style={styles.container}>
+      <ScrollViewExample />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 600,
+    marginTop: 150,
+    backgroundColor: '#800080',
+  }
+});
