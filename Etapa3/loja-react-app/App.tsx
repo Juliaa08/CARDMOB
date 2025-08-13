54c7d7fda51af1d7af3b4cdfaf7f2852eca49244
Julia Freitas <Julia.souto@aluno.ifsp.edu.br>
2025-08-13 10:27:37 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
-// import HomeScreen from './src/screens/HomeScreen';
-import RootNavigator from './src/navigation/RootNavigator';
+import HomeScreen from './src/screens/HomeScreen';
+
 export default function App() {
   return (
     <ThemeProvider>
-      <RootNavigator />
+      <HomeScreen />
     </ThemeProvider>
   );
 }
