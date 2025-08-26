c872464acf99d68fe50891fb305fcc2744c02254
Julia Freitas <Julia.souto@aluno.ifsp.edu.br>
2025-08-26 10:08:14 -0300

    Navegação Logada e não logada

--- Etapa3/loja-react-app/src/navigation/types.ts
+++ Etapa3/loja-react-app/src/navigation/types.ts
@@ -11,3 +11,16 @@ export type RootStackParamList = {
   Details: { itemId: number };
   Login: undefined;
 }
+
+export type AuthTabParamList = {
+  Home: undefined;
+  Settings: undefined;
+
+}
+
+
+export type AuthStackParamList = {
+  Tabs: NavigatorScreenParams<AuthTabParamList>;
+  Details: { itemId: number};
+  
+}
