96a58c6ed42978f214fea2bf41a2e32191af2dd1
Julia Freitas <Julia.souto@aluno.ifsp.edu.br>
2025-09-16 08:26:47 -0300

    Tela de catálogo com backend

--- Etapa3/loja-react-app/src/services/authService.ts
+++ Etapa3/loja-react-app/src/services/authService.ts
@@ -1,3 +1,8 @@
+import Config from  "react-native-config" ;
+
+// const API_URL = Config.API_URL;
+const API_URL = 'http://10.81.205.50:5000';
+
 export async function fakeLogin(email: string, password: string): Promise<string> {
     if (email === 'teste@example.com' && password === '123') {
         return Promise.resolve('fake-jwt-token');
@@ -7,26 +12,22 @@ export async function fakeLogin(email: string, password: string): Promise<string
 
 export async function requestLogin(email: string, password: string): Promise<string> {
     console.log(API_URL);
-    if (email === 'admin@example.com' && password === 'admin123') {
-        try {
-            const response = await fetch(`${API_URL}/api/users/login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({email, password}),
-            });
-            const data = await response.json();
-            const jwt = data.accessToken;
-            console.log(jwt);
-            return Promise.resolve(jwt);
-        }
-        catch (error) {
-            console.error(error);
-            return Promise.reject(error);
-        }
+    try {
+        const response = await fetch(`${API_URL}/api/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+        });
+        const data = await response.json();
+        const jwt = data.accessToken;
+        console.log(jwt);
+        return Promise.resolve(jwt);
     }
-
-
-    return Promise.reject('Credenciais inválidas');
+    catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+        // return Promise.reject('Credenciais inválidas');
+    }
 }
